04f61ed9177541e2648335d84e592b05a0f6a615
RahmaAhmed22 <ahmedrahma589@gmail.com>
2026-05-04 11:11:04 +0300

    fix issues with nginx

--- front-end/src/utils/constants.ts
+++ front-end/src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const USER_API = "/api/user";
-export const PRODUCT_API = "/api/product";
-export const CART_API = "/api/cart";
+export const USER_API = "/api/user/";
+export const PRODUCT_API = "/api/product/";
+export const CART_API = "/api/cart/";
